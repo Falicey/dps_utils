4578586e445c20e764992b71b2c40aade3df399a
alice.tarrant <alice.tarrant@du.edu>
2020-02-25 14:24:34 -0700

    -saves in excel format -formats worksheet to make data easier to read from the start

--- make_directory_sheet.py
+++ make_directory_sheet.py
@@ -2,8 +2,20 @@ import argparse
 from datetime import datetime
 import os
 import os.path
-import csv
+from openpyxl import Workbook
+from openpyxl.styles import NamedStyle
 import math
+
+
+def format_datetime(dt_float):
+    return datetime.fromtimestamp(dt_float)
+
+
+def format_size(size_bytes):
+    if size_bytes == 0:
+        return ""
+    else:
+        return math.ceil(size_bytes / 1000)
 
 
 def main():
@@ -15,17 +27,26 @@ def main():
 
     file_list = [(f.name, f.stat()) for f in os.scandir(target_dir)]
 
-    content_list = [(f[0], math.ceil(f[1].st_size / 1000), datetime.fromtimestamp(f[1].st_mtime),
-                     datetime.fromtimestamp(f[1].st_ctime)) for f in file_list]
+    content_list = [(f[0], f[1].st_size / 1000, format_datetime(f[1].st_ctime),
+                     format_datetime(f[1].st_mtime)) for f in file_list]
     content_list.sort(key=lambda a: a[0])
 
-    cvspath = os.path.join(target_dir, "Directory List.csv")
+    excelpath = os.path.join(target_dir, "Directory List.xlsx")
 
-    with open(cvspath, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, dialect='excel-tab')
-        csvwriter.writerow(("Name", "Size (KB)", "Date Modified", "Date Created"))
-        for c in content_list:
-            csvwriter.writerow(c)
+    wb = Workbook()
+    ws = wb.active
+    ws.page_setup.fitToWidth = 1
+    ws.append(("Name", "(Size (KB)", "Date Created", "Date Modified"))
+    date_style = NamedStyle(name='date_style', number_format='DD/MM/YYYY HH:MM:MM')
+    wb.add_named_style(date_style)
+    for i in ('A', 'B', 'C', 'D'):
+        ws.column_dimensions[i].width = '20'
+    for i in range(len(content_list)):
+        for j in range(4):
+            cell = ws.cell(i+2, j+1)
+            cell.value = content_list[i][j]
+
+    wb.save(excelpath)
 
 
 if __name__ == '__main__':
